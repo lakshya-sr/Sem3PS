efc8e3788b4e56f33874c21d3f369d80e10c5f8c
Lakshya Shrivastav <lakshyasrivastavc@gmail.com>
2025-08-25 08:50:55 +0530

    Update 21_my1stcontest.cpp

---
+++ basic_programming/21_my1stcontest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, a, b;
+		cout << n - a << " " << n - a - b << endl;
+	}
+}
+//
+//Format :
+//	Input : 
+//		First line contains T, the number of test cases
+//		Then the next T lines contain three numbers N, A, B separated by spaces
+//	Output:
+//		Output T lines containing the number of users who got rating and number of users getting rating greater than 1000
